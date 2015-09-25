460ae1e5ad7afa98ac5c8585a2e1343a54187e93
Neviim Jads <neviimdev@gmail.com>
2015-09-25 11:08:54 -0300

    modulo basico, log variaveis do sistema

--- README.md
+++ README.md
@@ -3,3 +3,42 @@ Script para ser usado no gron.
 Armazena por 48 horas os log de algumas variáveis do sistema
 (memoria, disco, processador), a cada 5 minutos é gerado um
 registro de 2k.
+
+
+
+# Configurar
+
+    Sugestão de que crie um diretorio em:
+
+        sudo mkdir /usr/local/src/serverlog
+        sudo cp ~/env/src/serverlog/getstatus.py /usr/local/src/serverlog
+
+
+    Em arquivo crontab acresentar esta linha:
+
+        # crontab -e
+
+                */5 * * * *  root python /usr/local/src/serverlog/getstatus.py
+
+
+    Comandos masicos do crom tab:
+
+        crontab -e      Edita o crontab atual do usuário
+        crontab -l       Exibe o atual conteúdo do crontab do usuário
+        crontab -r       Remove o crontab do usuário
+
+
+
+
+
+# V-0.1.0
+    - Definir o que sera implementado neste codigo inicial.
+    - Esta versão 0.1.0, tem por finalidade testar o conseito.
+    - Criar pasta no github deste projeto.
+    - Considera que tenha um banco mongoDB rodando.
+    - Escrever o basico, ler status desejado do sistema.
+    - Criar um banco no mongoDB de nome: logserver.
+    - No server que for ser monitorado colocar este escript no Cron.
+      */5 * * * *  root python /path/para/script_getdados.
+    - Efetuar os teste de gravação do log no server mongodb.
+    - Atualizar a documentação e commit.
---
+++ getstatus.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Neviim JAds'
+__versao__ = '0.1'
+
+import sys
+import psutil
+import socket
+import pymongo
+
+from datetime import datetime
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+
+
+def main():
+    try:
+        c = MongoClient(host="10.0.36.5", port=27017)   # aqui coloque o IP e Porta do seu servidor MongoDB.
+    except ConnectionFailure, e:
+        sys.stderr.write("Falha ao conectar no MongoDB: %s" % e)
+        sys.exit(1)
+
+    # abre o banco logserver
+    dbh = c["logserver"]
+
+    # se altenticação for true
+    if dbh.authenticate('neviim', 'logserver'):     # aqui coloque seu Usuario e Senha do banco MongoDB.
+        # le as informacoes do sistema.
+        cpu = psutil.cpu_times_percent()
+        disk_root = psutil.disk_usage('/')
+        ram = psutil.virtual_memory()
+        phymem = ram.total
+
+        # monta um dicionario com as informacoes.
+        doc = dict()
+        doc['server'] = socket.gethostname()
+        doc['date'] = datetime.now()
+        doc['disk_root'] = disk_root.free,
+        doc['phymem'] = ram.free
+        doc['cpu'] = {
+            'user': cpu.user,
+            'nice': cpu.nice,
+            'system': cpu.system,
+            'idle': cpu.idle,
+            'irq': cpu.irq
+        }
+        # escreve na collection neviim
+        dbh.neviim.insert(doc)      # aqui entra o nome da sua maqui monitorada, neviim dev é a minha.
+        #print "Successfully inserted document: %s" % doc
+
+main()
