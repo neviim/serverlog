39bc58ce90fbb1169571e1814ed26b00e991f69c
Neviim Jads <neviimdev@gmail.com>
2015-09-25 17:12:25 -0300

    Ajustes na função

--- getstatus.py
+++ getstatus.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# agente de captura
+
 __author__ = 'Neviim JAds'
 __versao__ = '0.1'
 
@@ -13,19 +15,8 @@ from datetime import datetime
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 
-
-def main():
-    try:
-        c = MongoClient(host="10.0.36.5", port=27017)   # aqui coloque o IP e Porta do seu servidor MongoDB.
-    except ConnectionFailure, e:
-        sys.stderr.write("Falha ao conectar no MongoDB: %s" % e)
-        sys.exit(1)
-
-    # abre o banco logserver
-    dbh = c["logserver"]
-
-    # se altenticação for true
-    if dbh.authenticate('neviim', 'logserver'):     # aqui coloque seu Usuario e Senha do banco MongoDB.
+# conect
+def monta_jason():
         # le as informacoes do sistema.
         cpu = psutil.cpu_times_percent()
         disk_root = psutil.disk_usage('/')
@@ -45,8 +36,29 @@ def main():
             'idle': cpu.idle,
             'irq': cpu.irq
         }
+        return doc
+
+# main
+def main():
+    try:
+        c = MongoClient(host="10.0.36.5", port=27017)   # aqui coloque o IP e Porta do seu servidor MongoDB.
+    except ConnectionFailure, e:
+        sys.stderr.write("Falha ao conectar no MongoDB: %s" % e)
+        sys.exit(1)
+
+    # abre o banco logserver
+    dbh = c["logserver"]
+
+    # se altenticação for true
+    if dbh.authenticate('neviim', 'logserver'):     # aqui coloque seu Usuario e Senha do banco MongoDB.
+
+        # monta o json das variaveis
+        pdoc = monta_jason()
+
         # escreve na collection neviim
-        dbh.neviim.insert(doc)      # aqui entra o nome da sua maqui monitorada, neviim dev é a minha.
+        dbh.neviim.insert(pdoc)      # aqui entra o nome da sua maqui monitorada, neviim dev é a minha.
         #print "Successfully inserted document: %s" % doc
 
-main()
+# --- inicio
+if __name__ == '__main__':
+    main()
